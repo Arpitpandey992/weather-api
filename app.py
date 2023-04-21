a523a8584cacd5502fcb52151dbcc41512c06b43
Arpitpandey992 <arpitpandey992@gmail.com>
2023-04-21 10:30:02 +0530

    feat: add city in query

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 from dotenv import load_dotenv
 
 from utils import get_temperature
-# Enter your OpenWeatherMap API key here
-
-load_dotenv()
 
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    load_dotenv()
+    DEFAULT_CITY = 'Roorkee'
 
+    # Routes
+    @app.route('/weather', methods=['GET'])
+    def getWeatherAPI():
+        city = request.args.get('city', DEFAULT_CITY)
+        return {"city": city, "weather": get_temperature(city)}
 
-@app.route('/weather', methods=['GET'])
-def getWeatherAPI():
-    return get_temperature('roorkee')
-
-
-if __name__ == '__main__':
-    app.run(debug=False)
+    return app
